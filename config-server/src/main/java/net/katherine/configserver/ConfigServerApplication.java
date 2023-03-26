0ae2c0a671e02dacd8b14150c52626290d84ab77
Katherine928 <katherine71317@gmail.com>
2023-03-26 16:24:26 -0400

    Add config server

---
+++ config-server/src/main/java/net/katherine/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package net.katherine.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
